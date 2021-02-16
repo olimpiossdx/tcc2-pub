50bbaa5999423f493fe62f5561a2318942d2d382
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-16 10:34:42 -0300

    finalização de layout do login

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -5,27 +5,22 @@ import LoginIMG from '../../assets/ufop2.png';
 
 const Login: React.FC = () => {
   return (<Grid container justifyContent='center' alignItems='center' style={{ height: 'calc(100vh - 64px)' }}>
-    <Grid item xs={11}>
-      <Grid container spacing={1} justifyContent='center' alignItems='center' style={{ height: '100%' }}>
-        <Paper>
-          <Grid item xs={11} sm={10} md={3}>
-            <Grid container spacing={1} alignItems='center'>
-              <Grid item xs={12} >
-                <img src={LoginIMG} alt='imagem de login' title='imagem de login' />
-              </Grid>
-              <Grid item xs={12}>
-                <Grid container justifyContent='flex-end'>
-                  <Grid item>
-                    <Button variant='contained'>inicie com Google</Button>
-                  </Grid>
-                </Grid>
+    <Paper component={Grid} style={{ padding: 10, margin: 5 }}>
+      <Grid item xs={12}>
+        <Grid container justifyContent='center' alignItems='center'>
+          <Grid item >
+            <img src={LoginIMG} alt='logo ufop' title='logo ufop' />
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container justifyContent='flex-end'>
+              <Grid item>
+                <Button variant='contained'>inciar com google</Button>
               </Grid>
             </Grid>
           </Grid>
-        </Paper>
+        </Grid>
       </Grid>
-
-    </Grid>
+    </Paper>
   </Grid>)
 }
 
