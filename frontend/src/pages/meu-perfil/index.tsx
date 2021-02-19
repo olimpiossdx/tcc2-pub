1a96d666f121f336c95fa10e6372035378f0f5f7
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-19 12:34:30 -0300

    Refatoração

--- frontend/src/pages/meu-perfil/index.tsx
+++ frontend/src/pages/meu-perfil/index.tsx
@@ -1,67 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Grid, Paper, Typography, Button, TextField } from '@material-ui/core';
 
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
 const MeuPefil: React.FC = () => {
+  const [disabled, setDisabled] = useState(true);
 
-  return (<Grid container justifyContent='center' alignItems='center' style={{ height: 'calc(100vh - 64px)' }}>
+  return (<Grid container justifyContent='center' alignItems='center'
+    style={{ height: 'calc(100vh - 64px)' }}>
     <Grid item xs={11} sm={10} md={6}>
-      <Paper style={{ padding: 10 }}>
-        <Grid container spacing={2} alignItems='center' style={{ height: '100%' }} >
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item >
-            <Typography align='left'>
-              Meu perfil
-           </Typography>
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              id="filled-disabled"
-              label="Nome"
-              defaultValue="Teste teste teste"
-              variant="outlined"
-              disabled
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              id="filled-disabled"
-              label="E-mail"
-              defaultValue="teste@example.com.br"
-              variant="outlined"
-              disabled
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              id="filled-disabled"
-              label="Chave de acesso"
-              defaultValue="76 3B 39 07"
-              variant="outlined"
-              disabled
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <Grid container justifyContent='flex-end'>
-              <Grid item>
-                <Button variant='outlined'>Agendar</Button>
-              </Grid>
-            </Grid>
-
-          </Grid>
+      <Grid component={Paper} container spacing={2} alignItems='center'
+        style={{ height: '100%', padding: '2%' }} >
+        <Grid item>
+          <AccountCircle />
         </Grid>
-      </Paper>
+        <Grid item >
+          <Typography align='left'>
+            Meu perfil
+           </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="filled-nome"
+            label="Nome"
+            defaultValue="Teste teste teste"
+            variant="outlined"
+            disabled={disabled}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="filled-email"
+            label="E-mail"
+            defaultValue="teste@example.com.br"
+            variant="outlined"
+            disabled={disabled}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="filled-chave-de-acesso"
+            label="Chave de acesso"
+            defaultValue="76 3B 39 07"
+            variant="outlined"
+            disabled={disabled}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          {disabled ?
+            (<Grid container justifyContent='flex-end'>
+              <Grid item>
+                <Button variant='outlined'
+                  onClick={() => setDisabled(false)}
+                >Editar</Button>
+              </Grid>
+            </Grid>) :
+            (<Grid container justifyContent='flex-end' spacing={1}>
+              <Grid item>
+                <Button variant='outlined' color='secondary' >Cancelar</Button>
+              </Grid>
+              <Grid item>
+                <Button variant='outlined' color='primary'>salvar</Button>
+              </Grid>
+            </Grid>)}
+        </Grid>
+      </Grid>
     </Grid>
   </Grid>)
 }
