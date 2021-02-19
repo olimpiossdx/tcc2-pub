0e0e989ca21afa4ba5a6583baa446972221dfad4
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-18 23:34:58 -0300

    Rename de css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import './App.css';
+import Routes from './routes';
+
+import './globalStyles.css';
 import { ThemeProvider } from '@material-ui/core';
 import DefaulTheme from './components/theme';
-import Routes from './routes';
 
 const App: React.FC = () =>
 (<ThemeProvider theme={DefaulTheme}>
