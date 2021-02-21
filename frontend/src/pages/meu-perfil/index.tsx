a9e950880dda68b9bab417a5b2d5821c10df3c41
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-21 09:28:19 -0300

    alterando funcionalidade de  edição

--- frontend/src/pages/meu-perfil/index.tsx
+++ frontend/src/pages/meu-perfil/index.tsx
@@ -10,8 +10,7 @@ const MeuPefil: React.FC = () => {
   return (<Grid container justifyContent='center' alignItems='center'
     style={{ height: 'calc(100vh - 120px)' }}>
     <Grid item xs={11} sm={10} md={6}>
-      <Grid component={Paper} container spacing={2} alignItems='center'
-        style={{ height: '100%', padding: '2%' }} >
+      <Grid component={Paper} container spacing={2} alignItems='center' style={{ height: '100%', padding: '2%' }} >
         <Grid item>
           <AccountCircle />
         </Grid>
@@ -23,52 +22,47 @@ const MeuPefil: React.FC = () => {
 
         <Grid item xs={12}>
           <TextField
+            id='filled-nome'
+            label='Nome'
+            defaultValue='Teste teste teste'
+            variant='outlined'
             fullWidth
-            id="filled-nome"
-            label="Nome"
-            defaultValue="Teste teste teste"
-            variant="outlined"
-            disabled={disabled}
-          />
+            disabled />
         </Grid>
 
         <Grid item xs={12}>
           <TextField
+            id='filled-email'
+            label='E-mail'
+            defaultValue='teste@example.com.br'
+            variant='outlined'
             fullWidth
-            id="filled-email"
-            label="E-mail"
-            defaultValue="teste@example.com.br"
-            variant="outlined"
-            disabled={disabled}
-          />
+            disabled />
         </Grid>
 
         <Grid item xs={12}>
           <TextField
+            id='filled-chave-de-acesso'
+            label='Chave de acesso'
+            defaultValue='76 3B 39 07'
+            variant='outlined'
             fullWidth
-            id="filled-chave-de-acesso"
-            label="Chave de acesso"
-            defaultValue="76 3B 39 07"
-            variant="outlined"
-            disabled={disabled}
-          />
+            disabled={disabled} />
         </Grid>
 
         <Grid item xs={12}>
           {disabled ?
             (<Grid container justifyContent='flex-end'>
               <Grid item>
-                <Button variant='outlined'
-                  onClick={() => setDisabled(false)}
-                >Editar</Button>
+                <Button variant='outlined' onClick={() => setDisabled(false)}>Editar</Button>
               </Grid>
             </Grid>) :
             (<Grid container justifyContent='flex-end' spacing={1}>
               <Grid item>
-                <Button variant='outlined' color='secondary' >Cancelar</Button>
+                <Button variant='outlined' color='secondary' onClick={() => setDisabled(true)} >Cancelar</Button>
               </Grid>
               <Grid item>
-                <Button variant='outlined' color='primary'>salvar</Button>
+                <Button variant='outlined' color='primary' >salvar</Button>
               </Grid>
             </Grid>)}
         </Grid>
