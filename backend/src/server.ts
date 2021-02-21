560e4284abf8b886b1c34bfbad2875021e7c4758
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-21 17:20:03 -0300

    Adicionando configurações iniciais do back-end, web api

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_request, response) => {
+  return response.json({ message: 'Hello back-end' });
+});
+
+/* eslint no-console: ["error", { allow: ["log"] }] */
+app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
