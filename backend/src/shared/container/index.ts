b1156c8017cd47021c04b72886fde61e1a866862
Olimpio Pimenta <olimpiodx@gmail.com>
2021-03-07 21:13:06 -0300

    Adicionando container

---
+++ backend/src/shared/container/index.ts
@@ -0,0 +1,10 @@
+// Todo: Adicionar importão de repostory e instalar lib
+//import { container } from 'tsyringe';
+
+//container.registerSingleton<any>('AgendamentosRepository', {});
+
+function teste(){
+
+};
+
+export default teste;
