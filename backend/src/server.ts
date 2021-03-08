63228880e92e373ff2d1260005feb3ea310f8f67
Olimpio Pimenta <olimpiodx@gmail.com>
2021-03-07 21:05:09 -0300

    Adicioanndo camada de erro

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,22 @@
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
 import routes from './routes';
+import AppError from './shared/erros';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+
+app.use((error: Error, request: Request, response: Response, _: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ status: 'error', message: error.message });
+  };
+
+  console.error('Internal server error', error);
+
+  return response.status(500).json({ status: 'error', message: 'Internal server error' });
+});
 
 app.get('/', (_request, response) => {
   return response.json({ message: 'Hello back-end' });
