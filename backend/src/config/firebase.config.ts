85ab82fba6a0375701ac570212e0e6340a645e03
Olimpio Pimenta <olimpiodx@gmail.com>
2021-04-05 22:38:52 -0300

    Adicionando configurações de conexão com firebase e acesso aos documentos

---
+++ backend/src/config/firebase.config.ts
@@ -0,0 +1,10 @@
+import firebaseAdmin from 'firebase-admin';
+import serviceAccount from "../config/serviceAccountKey.json";
+
+firebaseAdmin.initializeApp({ credential: firebaseAdmin.credential.cert(serviceAccount), databaseURL: process.env.APP_DATABASE_URL });
+
+export const firebaseDatabase = firebaseAdmin.database();
+export const documentBlocoRef = firebaseDatabase.ref('/bloco');
+export const documentUsersRef = firebaseDatabase.ref('/usuarios');
+export const documentAgendamentosRef = firebaseDatabase.ref('/agendamento');
+
