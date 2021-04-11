7b07e32910a01595222a784daa34bf8eb696c9d8
Olimpio Pimenta <olimpiodx@gmail.com>
2021-04-11 12:03:02 -0300

    Adicionando configurações de acesso ao firebase admin

--- backend/src/config/firebase.config.ts
+++ backend/src/config/firebase.config.ts
@@ -1,8 +1,12 @@
-import firebaseAdmin from 'firebase-admin';
+import * as firebaseAdmin from 'firebase-admin';
 import serviceAccount from "../config/serviceAccountKey.json";
 
-firebaseAdmin.initializeApp({ credential: firebaseAdmin.credential.cert(serviceAccount), databaseURL: process.env.APP_DATABASE_URL });
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount),
+  databaseURL: process.env.APP_DATABASE_URL
+});
 
+export const admin = firebaseAdmin;
 export const firebaseDatabase = firebaseAdmin.database();
 export const documentBlocoRef = firebaseDatabase.ref('/bloco');
 export const documentUsersRef = firebaseDatabase.ref('/usuarios');
