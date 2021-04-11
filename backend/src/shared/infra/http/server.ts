c6d4b07f432ae41e88c0fd809e3c368973236dd9
Olimpio Pimenta <olimpiodx@gmail.com>
2021-04-11 12:02:07 -0300

    Adicionando para camada de erros assíncronos

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,3 +1,6 @@
+import 'dotenv/config';
+import 'express-async-errors';
+
 import express, { NextFunction, Response, Request } from 'express';
 import cors from 'cors';
 import routes from './routes';
@@ -8,21 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-
-app.use((error: Error, request: Request, response: Response, _: NextFunction) => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({ status: 'error', message: error.message });
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ status: 'error', message: err.message });
   };
 
-  console.error('Internal server error', error);
+  console.error('Internal server error', err);
 
   return response.status(500).json({ status: 'error', message: 'Internal server error' });
 });
 
-app.get('/', (_request, response) => {
-  return response.json({ message: 'Hello back-end' });
-});
 
 /* eslint no-console: ["error", { allow: ["log"] }] */
 app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
