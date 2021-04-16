9588a9b5d0e82a93f7f42405d9840df9c5187291
Olimpio Pimenta <olimpiodx@gmail.com>
2021-04-15 22:58:12 -0300

    Cobrindo todos os casos de teste do cadastro de usuário

--- backend/src/usuarios/services/CreateUsuarioService.spec.ts
+++ backend/src/usuarios/services/CreateUsuarioService.spec.ts
@@ -22,4 +22,10 @@ describe('Criar usuário', () => {
 
     await expect(createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste@email.com', acessKey: 'teste-key', urlImg: 'http://teste.com' })).rejects.toBeInstanceOf(AppError);
   });
+
+  it('Não é possível criar ususário com mesmo cartão de acesso.', async () => {
+    await createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste1@email.com', acessKey: 'teste-key', urlImg: 'http://teste.com' });
+
+    await expect(createUsuarioService.execute({ id: '1425368189', nome: 'teste', email: 'teste@email.com', acessKey: 'teste-key', urlImg: 'http://teste.com' })).rejects.toBeInstanceOf(AppError);
+  });
 });
