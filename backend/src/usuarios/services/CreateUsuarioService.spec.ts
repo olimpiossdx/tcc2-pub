44b49cba1b553eae131d17b7e415c4029283166b
Olimpio Pimenta <olimpiodx@gmail.com>
2021-04-17 14:25:00 -0300

    Fix rename

--- backend/src/usuarios/services/CreateUsuarioService.spec.ts
+++ backend/src/usuarios/services/CreateUsuarioService.spec.ts
@@ -12,20 +12,20 @@ describe('Criar usuário', () => {
   });
 
   it('Usuário criado com sucesso.', async () => {
-    const user = await createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste@email.com', accesskey: 'teste-key', urlImg: 'http://teste.com' });
+    const user = await createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste@email.com', accessKey: 'teste-key', urlImg: 'http://teste.com' });
 
     expect(user).toBe(undefined);
   });
 
   it('Não é possível criar o mesmo ususário mais de uma vez.', async () => {
-    await createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste@email.com', accesskey: 'teste-key', urlImg: 'http://teste.com' });
+    await createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste@email.com', accessKey: 'teste-key', urlImg: 'http://teste.com' });
 
-    await expect(createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste@email.com', accesskey: 'teste-key', urlImg: 'http://teste.com' })).rejects.toBeInstanceOf(AppError);
+    await expect(createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste@email.com', accessKey: 'teste-key', urlImg: 'http://teste.com' })).rejects.toBeInstanceOf(AppError);
   });
 
   it('Não é possível criar ususário com mesmo cartão de acesso.', async () => {
-    await createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste1@email.com', accesskey: 'teste-key', urlImg: 'http://teste.com' });
+    await createUsuarioService.execute({ id: '1425368188', nome: 'teste', email: 'teste1@email.com', accessKey: 'teste-key', urlImg: 'http://teste.com' });
 
-    await expect(createUsuarioService.execute({ id: '1425368189', nome: 'teste', email: 'teste@email.com', accesskey: 'teste-key', urlImg: 'http://teste.com' })).rejects.toBeInstanceOf(AppError);
+    await expect(createUsuarioService.execute({ id: '1425368189', nome: 'teste', email: 'teste@email.com', accessKey: 'teste-key', urlImg: 'http://teste.com' })).rejects.toBeInstanceOf(AppError);
   });
 });
