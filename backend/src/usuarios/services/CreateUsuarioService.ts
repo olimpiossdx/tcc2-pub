0e8e2f70aa0b09ac7510ba4e4a068947b58731db
Olimpio Pimenta <olimpiodx@gmail.com>
2021-04-26 07:18:43 -0300

    Padronização

--- backend/src/usuarios/services/CreateUsuarioService.ts
+++ backend/src/usuarios/services/CreateUsuarioService.ts
@@ -8,23 +8,23 @@ import IUsuariosRepository from '../repositories/IUsuariosRepository';
 class CreateUsuarioService {
   constructor(
     @inject('UsuariosRepository')
-    private usuariosRepository: IUsuariosRepository) { }
+    private usuariosRepository: IUsuariosRepository) { };
 
   public async ExecuteAsync({ id, nome, email, accessKey, urlImg }: ICreteUsuarioDTO): Promise<void> {
     const existeUsuario = await this.usuariosRepository.FindByAuthIdAsync(id);
 
     if (existeUsuario) {
       throw new AppError('Usuário já cadastrado.');
-    }
+    };
 
     const isUnicKey = await this.usuariosRepository.IsUnicKeyAsync(accessKey);
 
     if (isUnicKey) {
       throw new AppError('Chave de acesso ja cadastrada.');
-    }
+    };
 
     await this.usuariosRepository.CreateAsync({ id, nome, email, accessKey, urlImg });
-  }
-}
+  };
+};
 
 export default CreateUsuarioService;
--- backend/src/usuarios/services/UpdateUsuarioAccessKeyService.ts
+++ backend/src/usuarios/services/UpdateUsuarioAccessKeyService.ts
@@ -12,17 +12,17 @@ interface IRequest {
 class UpdateUsuarioAccessKeyService {
   constructor(
     @inject('UsuariosRepository')
-    private usuariosRepository: IUsuariosRepository) { }
+    private usuariosRepository: IUsuariosRepository) { };
 
   public async execute({ id, accessKey }: IRequest): Promise<void> {
     const existeUsuario = await this.usuariosRepository.FindByAuthIdAsync(id);
 
     if (!existeUsuario) {
       throw new AppError('Usuário não cadastrado.');
-    }
+    };
 
     await this.usuariosRepository.UpdateAccessKeyAsync( id, accessKey );
-  }
-}
+  };
+};
 
 export default UpdateUsuarioAccessKeyService;
