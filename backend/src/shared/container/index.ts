970c16aa099d6328859c332d6e3e4f02fc00a0c2
Olimpio Pimenta <olimpiodx@gmail.com>
2021-04-28 23:43:16 -0300

    Adicionando injeção de dependência de bloco

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -4,4 +4,8 @@ import { container } from 'tsyringe';
 import UsuariosRepository from '../../usuarios/infra/firebase/repositories/UsuariosRepository';
 import IUsuariosRepository from '../../usuarios/repositories/IUsuariosRepository';
 
+import IBLocoRepository from '../../bloco/repositories/IBlocoRepository';
+import BlocoRepository from '../../bloco/infra/firebase/repositories/BlocoRepository';
+
 container.registerSingleton<IUsuariosRepository>('UsuariosRepository', UsuariosRepository);
+container.registerSingleton<IBLocoRepository>('BlocoRepository', BlocoRepository);
