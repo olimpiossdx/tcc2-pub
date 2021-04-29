d903480712d1ddf3508889cf38b12753dddf1722
Olimpio Pimenta <olimpiodx@gmail.com>
2021-04-28 23:31:20 -0300

    Atualizando DTO para criar bloco

--- backend/src/bloco/dtos/ICreteBlocoDTO.ts
+++ backend/src/bloco/dtos/ICreteBlocoDTO.ts
@@ -1,8 +1,14 @@
 //TODO: alterar paras regras de BLOCO
 export default interface ICreteBlocoDTO {
-  id: string;
-  accessKey: string;
-  nome: string;
-  email: string;
-  urlImg: undefined | string;
+  blocos: [
+    {
+      nome: string,
+      laboratorios: [
+        {
+          nome: string,
+          numero: number
+        }
+      ]
+    }
+  ]
 };
