9c0314241cc5a5b6af826262b8778f0d722b72e3
Olimpio Pimenta <olimpiodx@gmail.com>
2021-05-03 22:58:43 -0300

    Adicinando método get

--- backend/src/bloco/repositories/IBlocoRepository.ts
+++ backend/src/bloco/repositories/IBlocoRepository.ts
@@ -1,6 +1,7 @@
 import Bloco from '../infra/firebase/entities/Bloco';
 
 export default interface IBLocoRepository {
+  GetAsync(): Promise<Bloco[]>;
   FindAsync(id: string): Promise<Bloco | undefined>;
   CreateAsync(data: Bloco): Promise<void>;
   UpdateBlocoAsync(bloco: Bloco): Promise<void>;
