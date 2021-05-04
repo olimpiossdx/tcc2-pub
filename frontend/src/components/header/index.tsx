a5e8e51efdc60c7e55d777144ae8363ca67b9771
Olimpio Pimenta <olimpiodx@gmail.com>
2021-05-03 21:56:22 -0300

    Alterando  cabeçalho

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, IconButton, Typography, Avatar } from '@material-ui/core';
 
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import LogoIMG from '../../assets/ufop4.png';
+import { ReactComponent as LogoIcon } from '../../assets/logoContent.svg';
 import { useAuth } from '../hooks/authentication';
 
 const Header: React.FC = () => {
   const { user } = useAuth();
   return (<AppBar position='static' >
     <Toolbar>
-      <img alt='logo da UFOP' title='Universidadade Ferderal do Outro Preto' height='56' src={LogoIMG} />
+      <LogoIcon />
       <Typography variant='h6' color='inherit' component='div' noWrap style={{ flexGrow: 1, marginLeft: 5 }}>
         Universidade Federal Do Ouro Preto
         </Typography>
