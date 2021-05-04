dc8480ac41f04a7d6cf63b003d122bcc4762640b
Olimpio Pimenta <olimpiodx@gmail.com>
2021-05-03 22:49:56 -0300

    Adicionando get de laboratórios

--- backend/src/bloco/infra/firebase/repositories/BlocoRepository.ts
+++ backend/src/bloco/infra/firebase/repositories/BlocoRepository.ts
@@ -9,12 +9,44 @@ export interface objecToArray {
 
 class BlocosRepository implements IBlocoRepository {
   private blocosRepository: database.Reference;
+
   constructor() {
     this.blocosRepository = firebaseDatabase.ref('blocos');
   };
 
+  public async GetAsync(id: string): Promise<Bloco[]> {
+    // TODO: ajustar retorno para lista
+    const response = await this.blocosRepository.orderByChild('id').get();
+    let bloco: Bloco | undefined = new Bloco();
+
+    if (response.exists()) {
+      const blocoJson = response.toJSON() as objecToArray;
+
+      const hashkey = Object.keys(blocoJson)[0];
+      Object.assign(bloco, blocoJson[hashkey]);
+
+    } else {
+      bloco = undefined;
+    };
+
+    return new Array<Bloco>();
+  };
+
   public async FindAsync(id: string): Promise<Bloco | undefined> {
-    throw new Error('Method not implemented.');
+    const response = await this.blocosRepository.orderByChild('id').equalTo(id).get();
+    let bloco: Bloco | undefined = new Bloco();
+
+    if (response.exists()) {
+      const blocoJson = response.toJSON() as objecToArray;
+
+      const hashkey = Object.keys(blocoJson)[0];
+      Object.assign(bloco, blocoJson[hashkey]);
+
+    } else {
+      bloco = undefined;
+    };
+
+    return bloco;
   };
 
   public async UpdateBlocoAsync(bloco: Bloco): Promise<void> {
