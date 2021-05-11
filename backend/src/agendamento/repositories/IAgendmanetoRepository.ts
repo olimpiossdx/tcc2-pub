18b25e2b0f4b5de398e6b12ef273fc1b28299ab9
Olimpio <olimpiodx@gmail.com>
2021-05-11 11:24:25 -0300

    Adicionando testes para atualização de agendamentos

--- backend/src/agendamento/repositories/IAgendmanetoRepository.ts
+++ backend/src/agendamento/repositories/IAgendmanetoRepository.ts
@@ -5,5 +5,5 @@ export default interface IAgendamentoRepository {
   FindAsync(id: string): Promise<Agendamento | undefined>;
   FindSpecificAsync(data: Date, blocoId: string, laboratorioId: string, horarioInicio: Date, horarioFim: Date): Promise<Agendamento | undefined>;
   CreateAsync(data: Agendamento): Promise<Agendamento>;
-  UpdateAgendamentoAsync(agendamento: Agendamento): Promise<void>;
+  UpdateAgendamentoAsync(agendamento: Agendamento): Promise<Agendamento>;
 };
