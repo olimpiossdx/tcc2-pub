d2984b348e52255b2ec2a101c0bd71fe7d9ab3bc
Olimpio <olimpiodx@gmail.com>
2021-05-11 11:24:11 -0300

    Adicionando testes para atualização de agendamentos

--- backend/src/agendamento/services/UpdateAgendamentoService.spec.ts
+++ backend/src/agendamento/services/UpdateAgendamentoService.spec.ts
@@ -16,6 +16,48 @@ describe('Atualizar agendamento', () => {
   });
 
   it('Bloco atualizado com sucesso.', async () => {
+      let agendamento: ICreteAgendamentoDTO = {
+      bloco: {
+        id: 'teste-criar-agendamento-bloco',
+        nome: 'teste-criar-agendamento-bloco-nome'
+      },
 
+      laboratorio: {
+        id: 'teste-criar-agendamento-laboratorio',
+        nome: 'teste-criar-agendamento-laboratorio-nome',
+        numero: 103
+      },
+
+      data: new Date(2021, 6, 2, 12, 20, 0).getTime(),
+      horarioInicio: new Date(2021, 6, 2, 12, 20, 0).getTime(),
+      horarioFim: new Date(2021, 6, 2, 12, 55, 0).getTime(),
+    };
+    
+    const novoAgendamento = await createAgendamentoService.ExecuteAsync(agendamento);
+    novoAgendamento.horarioFim =new Date(2021, 6, 2, 12, 51, 0).getTime();  
+    await expect(updateAgendamentoService.execute(novoAgendamento)).toMatchObject(agendamento);
+  });
+  
+  it('Bloco atualizado com sucesso.', async () => {
+      let agendamento: ICreteAgendamentoDTO = {
+      bloco: {
+        id: 'teste-criar-agendamento-bloco',
+        nome: 'teste-criar-agendamento-bloco-nome'
+      },
+
+      laboratorio: {
+        id: 'teste-criar-agendamento-laboratorio',
+        nome: 'teste-criar-agendamento-laboratorio-nome',
+        numero: 103
+      },
+
+      data: new Date(2021, 6, 2, 12, 20, 0).getTime(),
+      horarioInicio: new Date(2021, 6, 2, 12, 20, 0).getTime(),
+      horarioFim: new Date(2021, 6, 2, 12, 55, 0).getTime(),
+    };
+    
+    const novoAgendamento = await createAgendamentoService.ExecuteAsync(agendamento);
+    novoAgendamento.horarioFim =new Date(2021, 6, 2, 12, 51, 0).getTime();  
+    expect(await fakeAgendamentoRepository.FindAsync(novoAgendamento.id)).toMatchObject(agendamento);
   });
 });
