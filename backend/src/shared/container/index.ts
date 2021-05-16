40346a770f2d9ec138f64f8083b8fc0543938bdf
Olimpio Pimenta <olimpiodx@gmail.com>
2021-05-16 01:15:47 -0300

    Atualizando depêndencias do projeto

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -6,5 +6,14 @@ import IUsuariosRepository from '../../usuarios/repositories/IUsuariosRepository
 import IBLocoRepository from '../../bloco/repositories/IBlocoRepository';
 import BlocoRepository from '../../bloco/infra/firebase/repositories/BlocoRepository';
 
+import IAgendamentoRepository from '../../agendamento/repositories/IAgendmanetoRepository';
+import AgendamentoRepository from '../../agendamento/infra/firebase/repositories/AgendamentoRepository';
+
+import IParametroPeriodoAgendamentoRepository from '../../parametro-periodo-agendamento/repositories/IParametroPeriodoAgendamentoRepository';
+import ParametroPeriodoAgendamentoRepository from '../../parametro-periodo-agendamento/infra/firebase/repositories/parametroPeriodoAgendamentoRepository';
+
+
 container.registerSingleton<IUsuariosRepository>('UsuariosRepository', UsuariosRepository);
 container.registerSingleton<IBLocoRepository>('BlocoRepository', BlocoRepository);
+container.registerSingleton<IAgendamentoRepository>('AgendamentoRepository', AgendamentoRepository);
+container.registerSingleton<IParametroPeriodoAgendamentoRepository>('ParametroPeriodoAgendamentoRepository', ParametroPeriodoAgendamentoRepository);
