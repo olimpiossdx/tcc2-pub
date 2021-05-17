034b7a56bf3107f3aa13556f4dc2a23fddc688ad
Olimpio <olimpiodx@gmail.com>
2021-05-17 12:51:12 -0300

    Update CreateParametroPeriodoAgendamentoService.spec.ts

--- backend/src/parametro-periodo-agendamento/services/CreateParametroPeriodoAgendamentoService.spec.ts
+++ backend/src/parametro-periodo-agendamento/services/CreateParametroPeriodoAgendamentoService.spec.ts
@@ -14,25 +14,12 @@ describe('Criar parametro de periodo para agendamento', () => {
   });
 
   it('Parametro de periodo para agendamento criado com sucesso.', async () => {
-    // TODO: ajustar teste
-    // const agendamento: ICreteParametroPeriodoAgendamentoDTO = {
-    //   bloco: {
-    //     id: 'teste-criar-agendamento-bloco',
-    //     nome: 'teste-criar-agendamento-bloco-nome'
-    //   },
+     const parametroPeriodoAgendamento: ICreteParametroPeriodoAgendamentoDTO = {
+       id:'teste-parametro-periodo-agendamento',
+       periodo:50
+     };
 
-    //   laboratorio: {
-    //     id: 'teste-criar-agendamento-laboratorio',
-    //     nome: 'teste-criar-agendamento-laboratorio-nome',
-    //     numero: 103
-    //   },
-
-    //   data: new Date(2021, 6, 2, 12, 20, 0).getTime(),
-    //   horarioInicio: new Date(2021, 6, 2, 12, 20, 0).getTime(),
-    //   horarioFim: new Date(2021, 6, 2, 12, 55, 0).getTime(),
-    // };
-
-    // const novoAgendamento = await createAgendamentoService.ExecuteAsync(agendamento);
-    // expect(await fakeAgendamentoRepository.GetByIdAsync<ParametroPeriodoAgendamento>(novoAgendamento.id)).toMatchObject(agendamento);
+     const novoParametroPeriodoAgendamento = await createAgendamentoService.ExecuteAsync(parametroPeriodoAgendamento);
+     expect(await fakeParametroPeriodoAgendamentoRepository.GetByIdAsync<ParametroPeriodoAgendamento>(novoParametroPeriodoAgendamento.id)).toMatchObject(parametroPeriodoAgendamento);
   });
 });
