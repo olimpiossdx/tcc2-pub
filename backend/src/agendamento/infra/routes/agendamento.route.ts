a0ba34a8de6ac984bf78f9a0c731c47b83f4a635
Olimpio Pimenta <olimpiodx@gmail.com>
2021-05-16 23:07:05 -0300

    Adicionando middlewre de validação de usuário

--- backend/src/agendamento/infra/routes/agendamento.route.ts
+++ backend/src/agendamento/infra/routes/agendamento.route.ts
@@ -1,31 +1,32 @@
 import { Router } from "express";
 import { celebrate, Segments, Joi } from 'celebrate';
 import AgendamentoController from "../http/controllers/AgendamentoController";
+import ensureAuthenticatedAsync from "../../../usuarios/infra/http/middlewares/ensureAuthenticated";
 
 const agendamentoRouter = Router();
 const agendamentoController = new AgendamentoController();
 
-agendamentoRouter.post('/',
+agendamentoRouter.post('/', ensureAuthenticatedAsync,
   celebrate({
     [Segments.BODY]: {
       agendamento: {
         nome: Joi.string().required(),
-        horarioInicio:Joi.string().required(),
-        horarioFim:Joi.string().required(),
+        horarioInicio: Joi.string().required(),
+        horarioFim: Joi.string().required(),
 
-        bloco:{
+        bloco: {
           id: Joi.string().required(),
           nome: Joi.string().required(),
-          numero:Joi.number().required()
+          numero: Joi.number().required()
         },
-        
-        laboratorio:{
+
+        laboratorio: {
           id: Joi.string().required(),
           nome: Joi.string().required(),
-          numero:Joi.number().required()
+          numero: Joi.number().required()
         }
       }
     }
   }), agendamentoController.CreateAsync);
-  
+
 export default agendamentoRouter;
--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import agendamentoRouter from "../../../../agendamento/infra/routes/agendamento.route";
 
 import blocosRouter from "../../../../bloco/infra/http/routes/blocos.routes";
+import parametroPeriodoAgendamentoRouter from "../../../../parametro-periodo-agendamento/infra/http/routes/parametroPeriodoAgendamento.routes";
 import authenticationRouter from "../../../../usuarios/infra/http/routes/authentication.routes";
 import usuariosRouter from "../../../../usuarios/infra/http/routes/usuarios.routes";
 
 
 const routes = Router();
 
+routes.use('/parametro-periodo-agendamento', parametroPeriodoAgendamentoRouter);
 routes.use('/agendamentos', agendamentoRouter);
 routes.use('/blocos', blocosRouter);
 
