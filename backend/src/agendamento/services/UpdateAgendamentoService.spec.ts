74008e1dbccecd86ce440a74affe085c36e06394
Olimpio Pimenta <olimpiodx@gmail.com>
2021-05-19 22:50:34 -0300

    Adicionando campo de id de usuário para atualização de agendamento

--- backend/src/agendamento/services/UpdateAgendamentoService.spec.ts
+++ backend/src/agendamento/services/UpdateAgendamentoService.spec.ts
@@ -1,29 +1,32 @@
-import { isEqual } from 'date-fns';
 import { uuid } from 'uuidv4';
-import ParametroPeriodoAgendamento from '../../parametro-periodo-agendamento/infra/firebase/entities/parametroPeriodoAgendamento';
-import FakeParametroPeriodoAgendamentoRepository from '../../parametro-periodo-agendamento/repositories/fakes/FakeParametroPeriodoAgendamentoRepository';
-import AppError from '../../shared/erros';
-import ICreteAgendamentoDTO from '../dtos/ICreteAgendamentoDTO';
 import Agendamento from '../infra/firebase/entities/Agendamento';
-import FakeBlocoRepository from '../repositories/fakes/FakeAgendamentoRepository';
+import AppError from '../../shared/erros';
 import CreateAgendamentoService from './CreateAgendamentoService';
+import FakeBlocoRepository from '../repositories/fakes/FakeAgendamentoRepository';
+import FakeParametroPeriodoAgendamentoRepository from '../../parametro-periodo-agendamento/repositories/fakes/FakeParametroPeriodoAgendamentoRepository';
+import FakeUsuariosRepository from '../../usuarios/repositories/fakes/FakesUsuariosRepository';
+import ICreteAgendamentoDTO from '../dtos/ICreteAgendamentoDTO';
+import ParametroPeriodoAgendamento from '../../parametro-periodo-agendamento/infra/firebase/entities/parametroPeriodoAgendamento';
 import UpdateAgendamentoService from './UpdateAgendamentoService';
 
 let fakeAgendamentoRepository: FakeBlocoRepository;
 let fakeParametroPeriodoAgendamentoRepository: FakeParametroPeriodoAgendamentoRepository;
 let createAgendamentoService: CreateAgendamentoService;
 let updateAgendamentoService: UpdateAgendamentoService;
+let fakeUsuarioRepository: FakeUsuariosRepository;
 
 describe('Atualizar agendamento', () => {
   beforeEach(() => {
     fakeAgendamentoRepository = new FakeBlocoRepository();
     fakeParametroPeriodoAgendamentoRepository = new FakeParametroPeriodoAgendamentoRepository();
-    createAgendamentoService = new CreateAgendamentoService(fakeAgendamentoRepository, fakeParametroPeriodoAgendamentoRepository);
+    fakeUsuarioRepository = new FakeUsuariosRepository();
+    createAgendamentoService = new CreateAgendamentoService(fakeAgendamentoRepository, fakeParametroPeriodoAgendamentoRepository, fakeUsuarioRepository);
     updateAgendamentoService = new UpdateAgendamentoService(fakeAgendamentoRepository, fakeParametroPeriodoAgendamentoRepository);
   });
 
   it('Agendamento atualizado com sucesso.', async () => {
     const agendamento: ICreteAgendamentoDTO = {
+      usuarioId: 'teste-usuario-id',
       bloco: {
         id: 'teste-criar-agendamento-bloco',
         nome: 'teste-criar-agendamento-bloco-nome',
@@ -44,6 +47,7 @@ describe('Atualizar agendamento', () => {
       horarioFim: new Date(2021, 6, 3, 12, 50, 0).getTime()
     };
 
+    await fakeUsuarioRepository.CreateAsync({ id: 'teste-usuario-id', email: 'teste@teste.com.br', accessKey: '14253678', nome: 'Teste usuario', urlImg: 'https://usuarioTeste.png' });
     await fakeParametroPeriodoAgendamentoRepository.CreateOrUpdateAsync({ periodo: 30 });
     const novoAgendamento = await createAgendamentoService.ExecuteAsync(agendamento);
 
@@ -58,6 +62,7 @@ describe('Atualizar agendamento', () => {
 
   it('Agendamento não atualizado.', async () => {
     let agendamento: ICreteAgendamentoDTO = {
+      usuarioId: 'teste-usuario-id',
       bloco: {
         id: 'teste-criar-agendamento-bloco',
         nome: 'teste-criar-agendamento-bloco-nome',
@@ -78,6 +83,7 @@ describe('Atualizar agendamento', () => {
       horarioFim: new Date(2021, 6, 5, 12, 50, 0).getTime()
     };
 
+    await fakeUsuarioRepository.CreateAsync({ id: 'teste-usuario-id', email: 'teste@teste.com.br', accessKey: '14253678', nome: 'Teste usuario', urlImg: 'https://usuarioTeste.png' });
     await fakeParametroPeriodoAgendamentoRepository.CreateOrUpdateAsync({ periodo: 30 });
     const novoAgendamento = await createAgendamentoService.ExecuteAsync(agendamento);
 
@@ -86,6 +92,7 @@ describe('Atualizar agendamento', () => {
 
   it('Não é possível atualizar o mesmo menor que período mínimo.', async () => {
     const agendamento: ICreteAgendamentoDTO = {
+      usuarioId: 'teste-usuario-id',
       bloco: {
         id: 'teste-criar-agendamento-bloco',
         nome: 'teste-criar-agendamento-bloco-nome',
@@ -106,6 +113,7 @@ describe('Atualizar agendamento', () => {
       horarioFim: new Date(2021, 6, 2, 12, 58, 0).getTime(),
     };
 
+    await fakeUsuarioRepository.CreateAsync({ id: 'teste-usuario-id', email: 'teste@teste.com.br', accessKey: '14253678', nome: 'Teste usuario', urlImg: 'https://usuarioTeste.png' });
     await fakeParametroPeriodoAgendamentoRepository.CreateOrUpdateAsync({ periodo: 35 });
     const entity = await createAgendamentoService.ExecuteAsync(agendamento);
 
@@ -119,6 +127,7 @@ describe('Atualizar agendamento', () => {
 
   it('Não é possível atualizar sem parâmeto de período para agendamento.', async () => {
     const agendamento: ICreteAgendamentoDTO = {
+      usuarioId: 'teste-usuario-id',
       bloco: {
         id: 'teste-criar-agendamento-bloco',
         nome: 'teste-criar-agendamento-bloco-nome',
@@ -138,7 +147,8 @@ describe('Atualizar agendamento', () => {
       horarioInicio: new Date(2021, 6, 2, 12, 20, 0).getTime(),
       horarioFim: new Date(2021, 6, 2, 12, 58, 0).getTime(),
     };
-
+    
+    await fakeUsuarioRepository.CreateAsync({ id: 'teste-usuario-id', email: 'teste@teste.com.br', accessKey: '14253678', nome: 'Teste usuario', urlImg: 'https://usuarioTeste.png' });
     const entityPeriodo = await fakeParametroPeriodoAgendamentoRepository.CreateOrUpdateAsync<ParametroPeriodoAgendamento>({ id: uuid(), periodo: 35 });
     const entity = await fakeAgendamentoRepository.CreateOrUpdateAsync<Agendamento>(agendamento);
 
