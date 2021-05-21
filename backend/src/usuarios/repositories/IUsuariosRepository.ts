2c44f24081e7e0f663769e51fdec91ebb6ba3d7b
Olimpio <olimpiodx@gmail.com>
2021-05-21 12:52:30 -0300

    Padronização para baseRepository

--- backend/src/usuarios/repositories/IUsuariosRepository.ts
+++ backend/src/usuarios/repositories/IUsuariosRepository.ts
@@ -1,8 +1,9 @@
 import Agendamento from '../../agendamento/infra/firebase/entities/Agendamento';
 import ICreteUsuarioDTO from '../dtos/ICreteUsuarioDTO';
 import Usuario from '../infra/firebase/entities/Usuario';
+import IBaseRepository from '../../shared/service/IBaseRepository';
 
-export default interface IUsuariosRepository {
+export default interface IUsuariosRepository extends IBaseRepository {
   FindByAuthIdAsync(authId: string): Promise<Usuario | undefined>;
   FindByEmailAsync(email: string): Promise<Usuario | undefined>;
   IsUnicKeyAsync(acessKey: string): Promise<boolean>;
