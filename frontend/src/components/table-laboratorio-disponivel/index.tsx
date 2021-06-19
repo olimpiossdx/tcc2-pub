2e0e48691281a90c9c743772dd733ced3d84f98d
Olimpio Pimenta <olimpiodx@gmail.com>
2021-06-19 11:00:25 -0300

    Criando estrutura para filtro por laboratório disponível

---
+++ frontend/src/components/table-laboratorio-disponivel/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { TableRow, TableCell, Typography } from '@material-ui/core';
+import { dataFormatter } from '../../utils/formatted';
+import FindInPageOutlinedIcon from '@material-ui/icons/FindInPageOutlined';
+import ILaboratorioDisponivel from '../../pages/laboratorio-disponiveis/model';
+
+const LaboratorioDisponivelRow: React.FC<ILaboratorioDisponivel> = ({ blocoId, laboratorioId, laboratorioNome, data, horarioInicio, horarioFim }) => {
+  return (<TableRow hover>
+    <TableCell component='th' scope='row'>
+      {laboratorioNome}
+    </TableCell>
+    <TableCell scope='row'>
+      {dataFormatter(data, {})}
+    </TableCell>
+    <TableCell >{`${dataFormatter(horarioInicio, { type: 'HH:mm' })} às ${dataFormatter(horarioFim, { type: 'HH:mm' })}`}</TableCell>
+  </TableRow>);
+};
+
+const TableLaboratorioDisponivel: React.FC<{ laboratoriosDisponiveis: ILaboratorioDisponivel[] }> = ({ laboratoriosDisponiveis }) => {
+  return (<>
+    {laboratoriosDisponiveis.length ? laboratoriosDisponiveis.map(laboratorioDisponivel => <LaboratorioDisponivelRow key={`${laboratorioDisponivel.blocoId}${laboratorioDisponivel.laboratorioId}`} {...laboratorioDisponivel} />) :
+      (<TableRow key='nenhum-registro'>
+        <TableCell scope='row' colSpan={3}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <FindInPageOutlinedIcon color='inherit' />
+            <Typography variant='h6' >
+              Nenhum agendamento encontrado.
+            </Typography>
+          </div>
+        </TableCell>
+      </TableRow>)}
+  </>);
+};
+
+export default TableLaboratorioDisponivel;
---
+++ frontend/src/pages/laboratorio-disponiveis/model.ts
@@ -0,0 +1,10 @@
+interface ILaboratorioDisponivel {
+  blocoId: string;
+  laboratorioId: string;
+  laboratorioNome: string;
+  data: string;
+  horarioInicio: string;
+  horarioFim: string
+};
+
+export default ILaboratorioDisponivel;
